a18a20f1ab2ecec4b914342e68d9400fdf806918
LuckyChimp <lukas.hartwich00@gmail.com>
2022-05-04 21:19:02 +0200

    First commit of whole project

---
+++ README.md
@@ -0,0 +1,110 @@
+# Frontend Mentor - Calculator app solution
+
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Link](#link)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users are able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+- **Bonus**: Continue calculating where they left off, even if the tab is already closed but the browser window is still open
+- **Bonus**: Always start with their favorite theme, because it will be saved even if the entire browser window is closed
+
+### Screenshot
+
+![Theme 1](./screenshot-theme1.png)
+![Theme 2](./screenshot-theme2.png)
+![Theme 3](./screenshot-theme3.png)
+
+### Link
+
+- Solution URL: [https://luckychimp.github.io/calculator/](https://luckychimp.github.io/calculator/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- SCSS for clean, easy and less CSS
+- custom HTML data attribute in combination with SCSS mixin and maps to handle the three different themes
+- Flexbox
+- CSS Grid
+- JavaScript: localStorage and sessionStorage to store the current calculation result and the current theme
+
+### What I learned
+
+In this project I learned how to use HTML, SCSS and JavaScript to add different color themes to the website.
+
+First, you should set a data-theme attribute in HTML for the body element with the name of the default theme. This is how I did it:
+```html
+<body data-theme="theme-1">
+```
+Then in JavaScript, for example when you click on a button or toggle button, you can set this data-theme attribute according to the name of the chosen theme:
+```js
+document.body.dataset.theme = name;
+```
+Then in SCSS it is best to create one or more files with the themes and their respective style rules. I created one file and it could look like this:
+```css
+$theme1: (
+    'clr_example': #3a4764,
+    'fs_example': 24px
+);
+
+$theme2: (
+  'clr_example': #3a4764,
+  'fs_example': 24px
+);
+
+$theme3: (
+  'clr_example': #3a4764,
+  'fs_example': 24px
+);
+```
+You can then access them from another SCSS file after importing them there with 
+```css
+@use './themes';
+```
+With the help of a mixin and the map function, the previously defined variables can now be inserted according to the currently selected theme via the data-theme attribute of the body element:
+```css
+@use 'sass:map';
+
+@mixin theme($name, $map) {
+    body[data-theme=#{$name}] {
+        background-color: map.get($map, 'clr_example');
+
+        .text {
+          font-size: map.get($map, 'fs_example');
+        }
+    }
+            
+```
+Now you only have to call the mixin for each theme:
+```css
+@include theme('theme-1', themes.$theme1);
+@include theme('theme-2', themes.$theme2);
+@include theme('theme-3', themes.$theme3);
+```
+
+### Useful resources
+
+- [Theming with Sass: An SCSS Tutorial](https://www.toptal.com/sass/theming-scss-tutorial) - This helped me to understand how to implement different color themes (or other themes in the future) in scss
+
+## Author
+
+- Frontend Mentor - [@LuckyChimp](https://www.frontendmentor.io/profile/LuckyChimp)
---
+++ scripts/main.js
@@ -0,0 +1,199 @@
+window.onload = () => {
+    // variables
+    var lastNumber;
+    var lastOperator;
+    var insertNewNumber = false;
+    var calcClickedRecently = false;
+
+    let themeSwitch = document.getElementsByClassName('theme-switch')[0];
+    let btnNumbers = document.getElementsByClassName('button-number');
+    let btnDot = document.getElementById('button-dot');
+    let btnPlus = document.getElementById('button-plus');
+    let btnMinus= document.getElementById('button-minus');
+    let btnTimes = document.getElementById('button-times');
+    let btnSlash = document.getElementById('button-slash');
+    let btnDelete = document.getElementById('button-delete');
+    let btnReset = document.getElementById('button-reset');
+    let btnCalculate = document.getElementById('button-calculate');
+    
+    // register event listeners
+    window.addEventListener('resize', initOutputWidth);
+    themeSwitch.addEventListener('click', onThemeSwitchClick);
+    for (let btnNumber of btnNumbers) {
+        btnNumber.addEventListener('click', onNumberClick);
+    }
+    btnDot.addEventListener('click', onDotClick);
+    btnPlus.addEventListener('click', onOperatorClick);
+    btnMinus.addEventListener('click', onOperatorClick);
+    btnTimes.addEventListener('click', onOperatorClick);
+    btnSlash.addEventListener('click', onOperatorClick);
+    btnDelete.addEventListener('click', deleteLastOutputChar);
+    btnReset.addEventListener('click', resetCalculation);
+    btnCalculate.addEventListener('click', showCalculationResult);
+    
+    init();
+
+    function init() {
+        initOutputWidth();
+        initTheme();
+        initResult();
+
+        function initTheme() {
+            let lastSavedTheme = localStorage.getItem('last-theme');
+            if (lastSavedTheme != null) { // set last saved theme if exists
+                changeTheme(lastSavedTheme);
+            }
+        }
+
+        function initResult() {
+            let lastSavedResult = sessionStorage.getItem('last-result');
+            if (lastSavedResult != null) { // show last result if exists
+                document.getElementById('result').innerText = lastSavedResult;
+            }
+        }
+    }
+
+    function initOutputWidth() {
+        let mainElmnt = document.querySelector('main');
+        let outputElmnt = document.getElementById('result');
+
+        outputElmnt.style.maxWidth = mainElmnt.clientWidth+'px';
+    }
+
+    function onThemeSwitchClick() {
+        switch (document.body.dataset.theme) {
+            case 'theme-1':
+                changeTheme('theme-2');
+                break;
+            case 'theme-2':
+                changeTheme('theme-3');
+                break;
+            case 'theme-3':
+                changeTheme('theme-1');
+                break;
+        }
+    }
+    
+    function changeTheme(name) {
+        let themeSwitchToggle = document.querySelector('.theme-switch .theme-switch-toggle');
+        switch(name) {
+            case 'theme-1':
+                document.body.dataset.theme = name;
+                themeSwitchToggle.style.marginLeft = '0rem';
+                localStorage.setItem('last-theme', name);
+                break;
+            case 'theme-2':
+                document.body.dataset.theme = name;
+                themeSwitchToggle.style.marginLeft = '0.6rem';
+                localStorage.setItem('last-theme', name);
+                break;
+            case 'theme-3':
+                document.body.dataset.theme = name;
+                themeSwitchToggle.style.marginLeft = '1.2rem';
+                localStorage.setItem('last-theme', name);
+                break;
+        }
+    }
+
+    function onNumberClick(event) {
+        let currentOutput = getOutput();
+        let number = {
+            'button-0': 0,
+            'button-1': 1,
+            'button-2': 2,
+            'button-3': 3,
+            'button-4': 4,
+            'button-5': 5,
+            'button-6': 6,
+            'button-7': 7,
+            'button-8': 8,
+            'button-9': 9,
+        }[event.target.id];
+
+        if (currentOutput == '0' || insertNewNumber) {
+            setOutput(number);
+            insertNewNumber = false;
+        } else {
+            setOutput(currentOutput.concat(number));
+        }
+    }
+
+    function onDotClick() {
+        let currentOutput = getOutput();
+        if (!currentOutput.includes('.')) { // only concat dot if one does not exist yet
+            setOutput(currentOutput.concat('.'));
+        }
+    }
+
+    function onOperatorClick(event) {
+        let operator = {
+            'button-plus': '+',
+            'button-minus': '-',
+            'button-times': '*',
+            'button-slash': '/'
+        }[event.target.id];
+        let currentNumber = getOutput();
+
+        if (lastNumber === undefined) {
+            lastNumber = currentNumber;
+        } else {
+            if (lastOperator !== undefined ) {
+                lastNumber = calculate(lastNumber, lastOperator, currentNumber);
+            }
+        }
+        lastOperator = operator;
+        insertNewNumber = true;
+        calcClickedRecently = false;
+    }
+
+    function showCalculationResult() {
+        if (!calcClickedRecently) {
+            let currentNumber = getOutput();
+            lastNumber = calculate(lastNumber, lastOperator, currentNumber);
+            setOutput(lastNumber);
+            calcClickedRecently = true;
+            lastOperator = undefined;
+        }
+    }
+
+    function calculate(number1, operator, number2) {
+        let result;
+        console.log(number1, operator, number2);
+        result = eval(number1 + operator + number2);
+        console.log('= ', result);
+        return result;
+    }
+
+    function deleteLastOutputChar() {
+        let currentOutput = getOutput();
+        let newOutput = currentOutput.substring(0, currentOutput.length-1);
+        if (newOutput.length != 0) { // only delete last char if new output is not empty and if its empty show 0
+            setOutput(newOutput);
+        } else {
+            setOutput(0);
+        }
+    }
+
+    function resetCalculation() {
+        setOutput(0);
+        // reset all global variables to default
+        lastNumber = undefined;
+        lastOperator = undefined;
+        insertNewNumber = false;
+    }
+
+    function getOutput() {
+        let outputElmnt = document.getElementById('result');
+        let output = outputElmnt.textContent;
+        return output;
+    }
+
+    function setOutput(text) {
+        // set output
+        let outputElmnt = document.getElementById('result');
+        outputElmnt.textContent = text;
+
+        // set the last result saved in session storage to new output
+        sessionStorage.setItem('last-result', text);
+    }
+}
